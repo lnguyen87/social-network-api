a4b55549895215f30e657ad274363e6acd6c1029
lnguyen87 <luc_nguyen@outlook.com>
2022-03-04 10:10:01 -0700

    set up user, thought, and reaction models

--- models/User.js
+++ models/User.js
@@ -1,8 +1,33 @@
 const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
 
 const UserSchema = new Schema ({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/]
+    },
+    thoughts: [ThoughtSchema],
 
+    friends: [UserSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
 });
 
 
